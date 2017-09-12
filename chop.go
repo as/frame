8e5305797aa2cf3526ffdf956ac0a960c1290b53
as <as.utf8@gmail.com>
2017-09-12 14:46:07 -0700

    clean clean.go and chop.go

--- chop.go
+++ chop.go
@@ -1,20 +1,14 @@
 package frame
 
 import (
-	"fmt"
 	"image"
 )
 
-func (f *Frame) setlines(s string, i int) {
-	fmt.Printf("[%p] %s\t%d ->%d\n", f, s, f.Nlines, i)
-	f.Nlines = i
-}
-
+// TODO(as): rename this to something more appropriate
 func (f *Frame) chopFrame(pt image.Point, p int64, bn int) {
 	for ; bn < f.Nbox; bn++ {
 		b := &f.Box[bn]
-		pt = f.lineWrap(pt, b)
-		if pt.Y >= f.r.Max.Y {
+		if pt = f.lineWrap(pt, b); pt.Y >= f.r.Max.Y {
 			break
 		}
 		p += int64(b.Len())
@@ -22,10 +16,8 @@ func (f *Frame) chopFrame(pt image.Point, p int64, bn int) {
 	}
 	f.Nchars = p
 	f.Nlines = f.maxlines
-	//	f.setlines("ChopFrame", f.maxlines)
 	if bn < f.Nbox {
 		f.Run.Delete(bn, f.Nbox-1)
 	}
 }
 
-// Put
--- clean.go
+++ clean.go
@@ -1,36 +1,31 @@
 package frame
 
 import (
-	"github.com/as/frame/box"
 	"image"
-	//"fmt"
 )
 
 func (f *Frame) clean(pt image.Point, n0, n1 int) {
-	var b *box.Box
 	c := f.r.Max.X
-	nb := n0
-	for ; nb < n1-1; nb++ {
-		b = &f.Box[nb]
-		pt = f.lineWrap(pt, b)
-		b1 := &f.Box[nb+1]
-		for b.Nrune >= 0 && nb < n1-1 && b1.Nrune >= 0 && pt.X+b.Width+b1.Width < c {
-			f.Merge(nb)
+	for ; n0 < n1-1; n0++ {
+		b0 := &f.Box[n0]
+		b1 := &f.Box[n0+1]
+		pt = f.lineWrap(pt, b0)
+		for b0.Nrune >= 0 && n0 < n1-1 && b1.Nrune >= 0 && pt.X+b0.Width+b1.Width < c {
+			f.Merge(n0)
 			n1--
-			b = &f.Box[nb]
 		}
-		pt = f.advance(pt, &f.Box[nb])
+		
+		pt = f.advance(pt, &f.Box[n0]) // dont simplify this
 	}
-
-	for ; nb < f.Nbox; nb++ {
-		b = &f.Box[nb]
-		pt = f.lineWrap(pt, b)
-		pt = f.advance(pt, &f.Box[nb])
+	
+	for ; n0 < f.Nbox; n0++ {
+		b0 := &f.Box[n0]
+		pt = f.lineWrap(pt, b0)
+		pt = f.advance(pt, b0)
 	}
+	
 	f.lastlinefull = 0
 	if pt.Y >= f.r.Max.Y {
 		f.lastlinefull = 1
 	}
-	//fmt.Printf("f.lastlinefull=%d\n",f.lastlinefull)
-	// Put
 }
