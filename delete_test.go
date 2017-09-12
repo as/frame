676e937419a13b08baa1b669d6d064befbcbc19b
as <as.utf8@gmail.com>
2017-09-12 15:20:56 -0700

    Add more tests

--- delete_test.go
+++ delete_test.go
@@ -1,17 +1,17 @@
 package frame
 
 import (
+	"github.com/as/etch"
+	"github.com/as/frame/font"
 	"image"
 	"image/draw"
 	"testing"
-	"github.com/as/etch"
-	"github.com/as/frame/font"
 )
 
 var (
-	R = image.Rect(0, 0, 232, 232)
+	R     = image.Rect(0, 0, 232, 232)
 	fsize = 16
-	ft = font.NewBasic(fsize)
+	ft    = font.NewBasic(fsize)
 )
 
 func abtest(r image.Rectangle) (fr0, fr1 *Frame, a, b *image.RGBA) {
@@ -22,8 +22,24 @@ func abtest(r image.Rectangle) (fr0, fr1 *Frame, a, b *image.RGBA) {
 	return fr0, fr1, a, b
 }
 
+func abtestbg(r image.Rectangle) (fa, fb *Frame, a, b *image.RGBA) {
+	fa, fb, a, b = abtest(r)
+	draw.Draw(a, a.Bounds(), fa.Color.Back, image.ZP, draw.Src)
+	draw.Draw(b, b.Bounds(), fb.Color.Back, image.ZP, draw.Src)
+	return fa, fb, a, b
+}
+
+func TestDeleteOneChar(t *testing.T) {
+	h, w, have, want := abtestbg(R)
+	h.Insert([]byte("1"), 0)
+	h.Delete(0, h.Len())
+	h.Untick()
+	w.Untick()
+	etch.Assert(t, have, want, "TestDelete.png")
+}
+
 func TestDeleteLastLineNoNL(t *testing.T) {
-	w, h, want, have := abtest(R)
+	w, h, want, have := abtestbg(R)
 	draw.Draw(want, want.Bounds(), w.Color.Back, image.ZP, draw.Src)
 	draw.Draw(have, have.Bounds(), h.Color.Back, image.ZP, draw.Src)
 	w.Insert([]byte("1234\ncccc\ndddd\n"), 0)
@@ -31,33 +47,7 @@ func TestDeleteLastLineNoNL(t *testing.T) {
 	h.Delete(5, 10)
 	w.Delete(5, 10)
 	// We can untick because have has an extra newline
-		h.Untick()
-		w.Untick()
-	etch.Assertf(t, have, want, "delta.png", "TestDeleteLastLineNoNL: failed")
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func abtestPad16(r image.Rectangle) (fr0, fr1 *Frame, a, b *image.RGBA) {
-	a = image.NewRGBA(r)
-	b = image.NewRGBA(r)
-	fr0 = New(r.Inset(fsize), font.NewBasic(fsize), a, A)
-	fr1 = New(r.Inset(fsize), font.NewBasic(fsize), b, A)
-	return fr0, fr1, a, b
+	h.Untick()
+	w.Untick()
+	etch.Assert(t, have, want, "TestDeleteLastLineNoNL.png")
 }
