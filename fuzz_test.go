9ce18e9fa8cd3d1e04a8224de924b74c469695e6
as <as.utf8@gmail.com>
2017-09-12 16:41:42 -0700

    add fuzz test for insert/delte

---
+++ fuzz_test.go
@@ -0,0 +1,88 @@
+package frame
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/as/etch"
+	"github.com/as/io/spaz"
+	"image"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestFuzz(t *testing.T) {
+	// The inverse of Insert is Delete. We can use this assumption
+	// to create a graphical fuzz test.
+	var err error
+	var n int
+	buf := make([]byte, 320*240)
+	N := 100 // number of rounds
+	sr := spaz.NewReader(bufio.NewReader(reader{}))
+	fr, fr2, a, b := abtestbg(image.Rect(0, 0, 320, 240))
+	for i := 0; i < N; i++ {
+		// Sync: Start by inserting something into to both frames
+		n, err = sr.Read(buf)
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+		in := buf[:n]
+		p0 := clamp(rand.Int63(), 0, fr.Len())
+		fr.Insert(in, p0)
+		fr2.Insert(in, p0)
+
+		// Insert something random into the first frame
+		// and see if Deleting that gets us coherent with the
+		// second frame
+
+		prior := buf[:n] // In case it fails, we need a record of the random data
+		n, err = sr.Read(buf)
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+		in = buf[:n]
+		p0 = clamp(rand.Int63(), 0, fr.Len())
+		ni := int64(fr.Insert(in, p0))
+		nd := fr.Delete(p0, p0+ni)
+		delta, ok := etch.Delta(a, b)
+		if !ok {
+			name := fmt.Sprintf("TestFuzz.Len%dIns%dCharsAt%d", len(prior), len(in), p0)
+			result := etch.Report(a, b, delta)
+			info := fmt.Sprintf("bufprior = %q\n", prior)
+			info += fmt.Sprintf("insert = %q\n", in)
+			info += fmt.Sprintf("p0 = %d\n", p0)
+			info += fmt.Sprintf("nins = %d\n", ni)
+			info += fmt.Sprintf("delete = %d:%d\n", p0, p0+ni)
+			info += fmt.Sprintf("ndel = %d\n", nd)
+			ioutil.WriteFile(name+".info", []byte(info), 0666)
+			t.Logf("see %s.png and %s.buf\n", name, name)
+			etch.WriteFile(t, name+".png", result)
+			t.FailNow()
+		}
+	}
+}
+func clamp(v, l, h int64) int64 {
+	if v < l {
+		return l
+	}
+	if v > h {
+		return h
+	}
+	return v
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+type reader struct {
+	ur io.Reader
+}
+
+func (reader) Read(p []byte) (n int, err error) {
+	return rand.Read(p)
+}
