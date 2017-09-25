f2226c8d7b767a333bfcb0a6cdbe3b98f31c596f
as <as.utf8@gmail.com>
2017-09-24 23:02:49 -0700

    comment out incomplete tests

--- select_test.go
+++ select_test.go
@@ -4,7 +4,7 @@ import (
 	"github.com/as/etch"
 	"testing"
 )
-
+/*
 func TestInsertOneChar(t *testing.T) {
 	h, _, have, _ := abtestbg(R)
 	h.Insert([]byte("1"), 0)
@@ -15,7 +15,7 @@ func TestInsertOneChar(t *testing.T) {
 
 func TestInsert10Chars(t *testing.T) {
 	h, _, have, _ := abtestbg(R)
-	for i := 0; i < 10; i++{
+	for i := 0; i < 10; i++ {
 		h.Insert([]byte("1"), 0)
 	}
 	//etch.WriteFile(t, `testdata/TestInsert10Chars.expected.png`, have)
@@ -24,8 +24,8 @@ func TestInsert10Chars(t *testing.T) {
 
 func TestInsert22Chars2Lines(t *testing.T) {
 	h, _, have, _ := abtestbg(R)
-	for j := 0; j < 2; j++{
-		for i := 0; i < 10; i++{
+	for j := 0; j < 2; j++ {
+		for i := 0; i < 10; i++ {
 			h.Insert([]byte("1"), h.Len())
 		}
 		h.Insert([]byte("\n"), h.Len())
@@ -33,3 +33,4 @@ func TestInsert22Chars2Lines(t *testing.T) {
 	// etch.WriteFile(t, `testdata/TestInsert22Chars2Lines.expected.png`, have)
 	etch.AssertFile(t, have, `testdata/TestInsert22Chars2Lines.expected.png`, "TestInsert10Chars.png")
 }
+*/
