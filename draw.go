b44e7041f4c40ae1a4d40fd2f42581dd45a88a26
as <as.utf8@gmail.com>
2017-10-08 02:18:28 -0700

    rm drawrun

--- draw.go
+++ draw.go
@@ -70,35 +70,6 @@ func (f *Frame) redrawRun0(r *box.Run, pt image.Point, text, back image.Image) {
 	}
 }
 
-func (f *Frame) drawRun(r *box.Run, pt image.Point) image.Point {
-	n := 0
-	for nb := 0; nb < r.Nbox; nb++ {
-		b := &r.Box[nb]
-		pt = f.wrapMin(pt, b)
-		if pt.Y == f.r.Max.Y {
-			r.Nchars -= r.Count(nb)
-			r.Delete(nb, r.Nbox-1)
-			break
-		}
-		if b.Nrune > 0 {
-			if n = f.fits(pt, b); n == 0 {
-				panic("drawRun: fits 0")
-			}
-			if n != b.Nrune {
-				r.Split(nb, n)
-				b = &r.Box[nb]
-			}
-			pt.X += b.Width
-		} else {
-			if b.BC == '\n' {
-				pt = f.wrap(pt)
-			} else {
-				pt.X += f.plot(pt, b)
-			}
-		}
-	}
-	return pt
-}
 
 func (f *Frame) drawsel(pt image.Point, p0, p1 int64, back, text image.Image) image.Point {
 	{ // doubled
