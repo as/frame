12cd24fd994ad923ce584959faf3cb1e285c8acd
as <as.utf8@gmail.com>
2017-10-09 04:31:20 -0700

    document pointof and indexof

--- pointof.go
+++ pointof.go
@@ -8,52 +8,8 @@ func (f *Frame) Grid(pt image.Point) image.Point {
 	return f.grid(pt)
 }
 
-func (f *Frame) IndexOf(pt image.Point) int64 {
-	pt = f.grid(pt)
-	qt := f.r.Min
-	p := int64(0)
-	bn := 0
-	for ; bn < f.Nbox && qt.Y < pt.Y; bn++ {
-		b := &f.Box[bn]
-		qt = f.wrapMax(qt, b)
-		if qt.Y >= pt.Y {
-			break
-		}
-		qt = f.advance(qt, b)
-		p += int64(b.Len())
-	}
-
-	for ; bn < f.Nbox && qt.X <= pt.X; bn++ {
-		b := &f.Box[bn]
-		qt = f.wrapMax(qt, b)
-		if qt.Y > pt.Y {
-			break
-		}
-		if qt.X+b.Width > pt.X {
-			if b.Nrune < 0 {
-				qt = f.advance(qt, b)
-			} else {
-				bs := f.newRulerFunc(b.Ptr, f.Font)
-				for {
-					size, width, err := bs.Next()
-					if err != nil {
-						break
-					}
-					qt.X += width
-					if qt.X > pt.X {
-						break
-					}
-					p += int64(size)
-				}
-			}
-		} else {
-			p += int64(b.Len())
-			qt = f.advance(qt, b)
-		}
-	}
-	return p
-}
-
+// PointOf returns the point on the frame's
+// bitmap that intersects the index p.
 func (f *Frame) PointOf(p int64) image.Point {
 	return f.pointOf(p, f.r.Min, 0)
 }
