245da84d9f1dc830dca874af3a82dd9f09fa47da
as <as.utf8@gmail.com>
2018-02-10 15:13:25 -0800

    frame: box: remove unneeded utility methods

--- draw.go
+++ draw.go
@@ -72,35 +72,54 @@ func (f *Frame) redrawRun0(r *box.Run, pt image.Point, text, back image.Image) i
 	return pt
 }
 
+// widthBox returns the width of box n. If the length of
+// alt is different than the box, alt is measured and
+// returned instead.
+func (f *Frame) widthBox(b *box.Box, alt []byte) int {
+	if b.Nrune < 0 || len(alt) == b.Len() {
+		return b.Width
+	}
+	return f.Face.Dx(alt)
+}
+
 func (f *Frame) drawsel(pt image.Point, p0, p1 int64, back, text image.Image) image.Point {
 	{
 		// doubled
 		p0, p1 := int(p0), int(p1)
 		q0 := 0
 		trim := false
-		defer f.Flush()
+
+		var (
+			nb int
+			b  *box.Box
+		)
+
+		for nb = 0; nb < f.Nbox; nb++ {
+			b = &f.Box[nb]
+			l := q0 + b.Len()
+			if l > p0 {
+				break
+			}
+			q0 = l
+		}
 
 		// Step into box, start coloring it
 		// How much does this lambda slow things down?
-		stepFill := func(bn int) {
+		stepFill := func() {
 			qt := pt
-			if pt = f.wrapMax(pt, (&f.Box[bn])); pt.Y > qt.Y {
+			if pt = f.wrapMax(pt, b); pt.Y > qt.Y {
 				r := image.Rect(qt.X, qt.Y, f.r.Max.X, pt.Y)
 				f.Draw(f.b, r, back, qt, f.op)
 				//f.Flush(r)
 			}
 		}
-		nb := 0
-		for ; nb < f.Nbox && q0+f.LenBox(nb) <= p0; nb++ {
-			// region -2: skip
-			q0 += f.LenBox(nb)
-		}
 
 		for ; nb < f.Nbox && q0 < p1; nb++ {
+			b = &f.Box[nb]
 			if q0 >= p0 { // region 0 or 1 or 2
-				stepFill(nb)
+				stepFill()
 			}
-			ptr := f.BoxBytes(nb)
+			ptr := b.Ptr[:b.Len()]
 			if q0 < p0 {
 				// region -1: shift p right inside the selection
 				ptr = ptr[p0-q0:]
@@ -114,12 +133,10 @@ func (f *Frame) drawsel(pt image.Point, p0, p1 int64, back, text image.Image) im
 				ptr = ptr[:lim]
 				trim = true
 			}
-			w := f.WidthBox(nb, ptr)
-			if f.PlainBox(nb) {
-
+			w := f.widthBox(b, ptr)
+			if b.Nrune > 0 {
 				f.Draw(f.b, image.Rect(pt.X, pt.Y, min(pt.X+w, f.r.Max.X), pt.Y+f.Font.Dy()), back, pt, f.op)
 				f.StringBG(f.b, pt, text, image.ZP, f.Font, ptr, back, image.ZP)
-				//TODO(as): bug is that when back==nil, StringBG turns into StringNBG and that doesn't work with replacement runes.
 			} else {
 				f.Draw(f.b, image.Rect(pt.X, pt.Y, min(pt.X+w, f.r.Max.X), pt.Y+f.Font.Dy()), back, pt, f.op)
 
@@ -131,8 +148,9 @@ func (f *Frame) drawsel(pt image.Point, p0, p1 int64, back, text image.Image) im
 			}
 		}
 
-		if p1 > p0 && nb != 0 && nb < f.Nbox && f.LenBox(nb-1) > 0 && !trim {
-			stepFill(nb)
+		if p1 > p0 && nb != 0 && nb < f.Nbox && f.Box[nb-1].Len() > 0 && !trim {
+			b = &f.Box[nb]
+			stepFill()
 		}
 		return pt
 	}
