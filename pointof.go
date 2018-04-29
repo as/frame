c4dc4626413277704d24d7e30e6dc16f7f7ebf59
as <as.utf8@gmail.com>
2018-04-29 00:09:10 -0700

    frame: allow query methods to succeed with the zero-valued return even if the frame is nil

--- pointof.go
+++ pointof.go
@@ -4,13 +4,19 @@ import (
 	"image"
 )
 
+// Grid returns a grid-aligned point on the frame relative to pt
 func (f *Frame) Grid(pt image.Point) image.Point {
+	if f == nil{
+		return image.ZP
+	}
 	return f.grid(pt)
 }
 
-// PointOf returns the point on the frame's
-// bitmap that intersects the index p.
+// PointOf returns the point on the closest to index p.
 func (f *Frame) PointOf(p int64) image.Point {
+	if f == nil{
+		return image.ZP
+	}
 	return f.pointOf(p, f.r.Min, 0)
 }
 
@@ -34,21 +40,6 @@ func (f *Frame) pointOf(p int64, pt image.Point, bn int) (x image.Point) {
 				if p > 0 {
 					pt.X += f.Face.Dx(ptr[:p])
 				}
-				//				bsb := len(ptr)
-				//				i := 0
-				//				for p > 0 {
-				//					if bsb == i {
-				//						break
-				//					}
-				//					size := 1
-				//					widthPx := f.Font.Dx(ptr[i : i+size])
-				//					i += size
-				//					p -= int64(size)
-				//					pt.X += widthPx
-				//					if pt.X > f.r.Max.X {
-				//						panic("PtOfCharPtBox")
-				//					}
-				//				}
 			}
 			break
 		}
